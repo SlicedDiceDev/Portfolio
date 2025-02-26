63ab6d77d70a1ade3fbd8c3b647a227be577634b
Ryan McCloud <ryan.mccloud@mymail.champlain.edu>
2025-02-26 13:53:53 -0500

    made iterations to ruins page
    took feedback from class and fully reworked how the ruins page is formatted. the only thing left to do is redo the reel to add more commentary

--- index.js
+++ index.js
@@ -71,3 +71,13 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+//image slider
+
+$("#slider").on("input change", (e)=>{
+  const sliderPos = e.target.value;
+  // Update the width of the foreground image
+  $('.foreground-img').css('width', `${sliderPos}%`)
+  // Update the position of the slider button
+  $('.slider-button').css('left', `calc(${sliderPos}% - 18px)`)
+});
